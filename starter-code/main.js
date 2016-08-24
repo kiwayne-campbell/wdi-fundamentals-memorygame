50d04fb05879d08a230e834d6888da7350f3b64e
KC <kc@KCs-MacBook-Air.local>
2016-08-24 13:41:34 +0100

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,8 @@ var cardone = "queen"
 var cardtwo = "queen"
 var cardthree = "king"
 var cardfour = "king"
+
+if (cardone === cardtwo && cardthree === cardfour) { 
+	alert("You Found a Match");
+}
+else {alert("Sorry, try again");}
